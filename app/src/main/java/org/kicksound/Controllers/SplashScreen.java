2bbdae52bde0039830790d209e5accba36798600
Unknown <alex-bad97@hotmail.fr>
2019-04-09 14:56:01 +0200

    Splash screen

---
+++ app/src/main/java/org/kicksound/Controllers/SplashScreen.java
@@ -0,0 +1,41 @@
+package org.kicksound.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import org.kicksound.R;
+import org.kicksound.Utils.HandleIntent;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(SPLASH_TIME_OUT);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}

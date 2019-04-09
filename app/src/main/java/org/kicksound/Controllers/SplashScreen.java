8bd5a238b15c381b743c2f8f70d81d0dd73793a1
Unknown <alex-bad97@hotmail.fr>
2019-04-09 19:51:30 +0200

    Splash screen + session authentification

--- app/src/main/java/org/kicksound/Controllers/SplashScreen.java
+++ app/src/main/java/org/kicksound/Controllers/SplashScreen.java
@@ -1,32 +1,56 @@
 package org.kicksound.Controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import es.dmoral.toasty.Toasty;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
+import org.kicksound.Models.Login;
 import org.kicksound.R;
+import org.kicksound.Services.AccountService;
 import org.kicksound.Utils.HandleIntent;
+import org.kicksound.Utils.RetrofitManager;
 
 public class SplashScreen extends AppCompatActivity {
-
-    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        RetrofitManager.getInstance().setUrl(getString(R.string.URL_API));
 
         Thread timer = new Thread() {
             public void run() {
-                try {
-                    sleep(SPLASH_TIME_OUT);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
+                SharedPreferences user_pref = getSharedPreferences(getString(R.string.USER_PREF), MODE_PRIVATE);
+                String userAccessToken = user_pref.getString(getString(R.string.userAccessToken), null);
+
+                if (userAccessToken != null) {
+                    RetrofitManager.getInstance().getRetrofit().create(AccountService.class)
+                            .accessTokenExist(userAccessToken)
+                            .enqueue(new Callback<Login>() {
+                                @Override
+                                public void onResponse(Call<Login> call, Response<Login> response) {
+                                    if(response.code() == 200) {
+                                        HandleIntent.redirectToAnotherActivity(SplashScreen.this, News.class, findViewById(R.id.splash_screen));
+                                    } else {
+                                        HandleIntent.redirectToAnotherActivity(SplashScreen.this, MainActivity.class, findViewById(R.id.splash_screen));
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<Login> call, Throwable t) {
+                                    Toasty.info(getApplicationContext(), getString(R.string.connexion_error), Toast.LENGTH_SHORT, true).show();
+                                }
+                            });
+                } else {
+                    HandleIntent.redirectToAnotherActivity(SplashScreen.this, MainActivity.class, findViewById(R.id.splash_screen));
                 }
             }
         };

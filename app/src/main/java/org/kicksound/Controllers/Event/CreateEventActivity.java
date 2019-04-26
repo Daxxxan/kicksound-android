999ffa1e50e9124658a3b44521bb7ba4d4dfa42e
Unknown <alex-bad97@hotmail.fr>
2019-04-26 12:09:59 +0200

    Delete some comments

--- app/src/main/java/org/kicksound/Controllers/Event/CreateEventActivity.java
+++ app/src/main/java/org/kicksound/Controllers/Event/CreateEventActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,15 +23,11 @@ import static org.kicksound.Utils.Class.HandleEditText.fieldIsEmpty;
 
 public class CreateEventActivity extends AppCompatActivity {
 
-    private static final int PICK_FROM_FILE = 1;
-    private static ImageView gallery_image;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
         createEvent();
-        //addPicture();
     }
 
     private void createEvent() {
@@ -81,86 +76,4 @@ public class CreateEventActivity extends AppCompatActivity {
     private EditText getEventTitleEditText() {
         return findViewById(R.id.eventName);
     }
-
-    /*private void addPicture() {
-        Button addEventPictureButton = findViewById(R.id.addEventPicture);
-        gallery_image = findViewById(R.id.eventPicture);
-        addEventPictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent selectPicture = new Intent();
-                selectPicture.setType("image/*");
-                selectPicture.setAction(Intent.ACTION_PICK);
-                startActivityForResult(Intent.createChooser(selectPicture, "Complete action using"), PICK_FROM_FILE);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_FROM_FILE) {
-            if (resultCode == RESULT_OK) {
-                try {
-
-                    Uri imageuri = data.getData();
-                    //String real_Path = getRealPathFromUri(CreateEventActivity.this,imageuri);
-
-                    Bitmap bitmap = decodeUri(CreateEventActivity.this, imageuri, 300);
-                    if (bitmap != null)
-                        gallery_image.setImageBitmap(bitmap);
-
-                    else
-                        Toast.makeText(CreateEventActivity.this,
-                                "Error while decoding image.",
-                                Toast.LENGTH_SHORT).show();
-                } catch (FileNotFoundException e) {
-
-                    e.printStackTrace();
-                    Toast.makeText(CreateEventActivity.this, "File not found.",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    public static Bitmap decodeUri(Context context, Uri uri, final int requiredSize) throws FileNotFoundException {
-        BitmapFactory.Options o = new BitmapFactory.Options();
-        o.inJustDecodeBounds = true;
-        BitmapFactory.decodeStream(context.getContentResolver()
-                .openInputStream(uri), null, o);
-
-        int width_tmp = o.outWidth, height_tmp = o.outHeight;
-        int scale = 1;
-
-        while (true) {
-            if (width_tmp / 2 < requiredSize || height_tmp / 2 < requiredSize)
-                break;
-            width_tmp /= 2;
-            height_tmp /= 2;
-            scale *= 2;
-        }
-
-        BitmapFactory.Options o2 = new BitmapFactory.Options();
-        o2.inSampleSize = scale;
-        return BitmapFactory.decodeStream(context.getContentResolver()
-                .openInputStream(uri), null, o2);
-    }
-
-    public static String getRealPathFromUri(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri, proj, null,
-                    null, null);
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }*/
 }

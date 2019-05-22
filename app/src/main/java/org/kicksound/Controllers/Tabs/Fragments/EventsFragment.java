1bd4a6de28e375dfebf9dbf77f88a5b2dbf510b5
Unknown <alex-bad97@hotmail.fr>
2019-05-22 18:07:50 +0200

    Event: Use FloatingActionMenu

--- app/src/main/java/org/kicksound/Controllers/Tabs/Fragments/EventsFragment.java
+++ app/src/main/java/org/kicksound/Controllers/Tabs/Fragments/EventsFragment.java
@@ -1,22 +1,20 @@
 package org.kicksound.Controllers.Tabs.Fragments;
 
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.fragment.app.Fragment;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 import org.kicksound.Controllers.Event.CreateEventActivity;
 import org.kicksound.R;
 import org.kicksound.Utils.Class.HandleAccount;
-import org.kicksound.Utils.Class.HandleFAB;
 import org.kicksound.Utils.Class.HandleIntent;
 import org.kicksound.Utils.Enums.UserType;
-
-import androidx.fragment.app.Fragment;
 
 public class EventsFragment extends Fragment {
 
@@ -38,25 +36,24 @@ public class EventsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_events_fragment, container, false);
 
-        if(HandleAccount.getUserType() != UserType.USER) {
-            RelativeLayout eventView = view.findViewById(R.id.event_fragment);
-            FloatingActionButton fabEventButton =  HandleFAB.createFabButtonOnBottomLeft(
-                    getContext(),
-                    R.drawable.ic_action_nplus,
-                    getResources().getColor(R.color.colorGold),
-                    ColorStateList.valueOf(getResources().getColor(R.color.colorGold)),
-                    R.id.eventFabButton
-            );
+        displayFabIfUserIsAnArtist(view);
 
-            fabEventButton.setOnClickListener(new View.OnClickListener() {
+        return view;
+    }
+
+    private void displayFabIfUserIsAnArtist(View view) {
+        if(HandleAccount.getUserType() != UserType.USER) {
+            FloatingActionMenu famEvent = view.findViewById(R.id.famEvent);
+            FloatingActionButton fabEvent = view.findViewById(R.id.fabCreateEvent);
+
+            famEvent.showMenu(true);
+
+            fabEvent.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     HandleIntent.redirectToAnotherActivity(getContext(), CreateEventActivity.class, v);
                 }
             });
-
-            eventView.addView(fabEventButton);
         }
-        return view;
     }
 }
